e18ddfcbc24801a261c76f91aad7c0fe19270612
Mohef <moheftech@gmail.com>
2026-05-12 15:26:03 +0700

    lesson 2: session dan cookies management

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import httpx
 import asyncio
+import json
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
     "Accept": "application/json, text/plain, */*",
 }
+
+async def save_session(client, filename="session.json"):
+    cookies = dict(client.cookies)
+    with open(filename, "w") as f:
+        json.dump(cookies, f)
+    print(f"session disimpan ke {filename}: {cookies}")
+
+async def load_session(client, filename="session.json"):
+    try:
+        with open(filename, "r") as f:
+            cookies = json.load(f)
+        for key, value in cookies.items():
+            client.cookies.set(key, value)
+        print(f"session di-load dari {filename}: {cookies}")
+        return True
+    except FileNotFoundError:
+        print(f" Belum ada session tersimpan, perlu login dulu!")
+        return false
 
 async def main():
     async with httpx.AsyncClient(headers=HEADERS) as client:
@@ -12,24 +31,22 @@ async def main():
         # Simulasi Login -> dapat cookies
         print("=== Step 1: LOGIN, dapat cookies===")
         await client.get("https://httpbin.org/cookies/set?bot_session=mohef123&user_id=99")
-        print(f"Cookies tersimpan: {dict(client.cookies)}")
+        await save_session(client)
 
-        # Simulasi: request berikutnya -> cookies otomatis terkirim
-        print("\n=== Step 2 : Request berikutnya, cookies otomatis terkirim ===")
-        response = await client.get("https://httpbin.org/cookies")
-        body = response.json()
-        print(f"Cookie yang diterima server: {body['cookies']}")
+    # simulasi bot restart, buat client baru, session kosong
+    print("\n=== Step 2: BOT RESTART, buat client baru, session kosong ===")
+    async with httpx.AsyncClient(headers=HEADERS) as client2:
 
-        print("\n=== Step 3 Verifikasi ===")
-        if body['cookies'].get('bot_session') == 'mohef123':
-            print("✅ session aktif - bot siap eksekusi")
-        else:
-            print("❌ Session hilang!")
+        # load session dari file
+        loaded = await load_session(client2)
 
-        # # Hit endpoint yang set cookie
-        # response = await client.get("https://httpbin.org/cookies/set?bot_session=abc123&user_id=42")
-        
-        # print("=== COOKIES TERSIMPAN ===")
-        # print(dict(client.cookies))
+        if loaded:
+            #verifikasi session masih valid
+            response = await client2.get("https://httpbin.org/cookies")
+            body = response.json()
+            print(f"\nCookie yang diterima server: {body['cookies']}")
 
+            if body['cookies'].get('bot_session'):
+                print("Session berhasil dipulihkan - siap war tanpa login ulang!")
+                
 asyncio.run(main())
