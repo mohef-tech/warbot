c8c4d9c49c0adcc04ece490fe728d62b186c35ab
Mohef <moheftech@gmail.com>
2026-05-12 14:59:26 +0700

    Step 2 — Cookie Otomatis Terkirim di Request Berikutnya

--- main.py
+++ main.py
@@ -9,10 +9,27 @@ HEADERS = {
 async def main():
     async with httpx.AsyncClient(headers=HEADERS) as client:
         
-        # Hit endpoint yang set cookie
-        response = await client.get("https://httpbin.org/cookies/set?bot_session=abc123&user_id=42")
+        # Simulasi Login -> dapat cookies
+        print("=== Step 1: LOGIN, dapat cookies===")
+        await client.get("https://httpbin.org/cookies/set?bot_session=mohef123&user_id=99")
+        print(f"Cookies tersimpan: {dict(client.cookies)}")
+
+        # Simulasi: request berikutnya -> cookies otomatis terkirim
+        print("\n=== Step 2 : Request berikutnya, cookies otomatis terkirim ===")
+        response = await client.get("https://httpbin.org/cookies")
+        body = response.json()
+        print(f"Cookie yang diterima server: {body['cookies']}")
+
+        print("\n=== Step 3 Verifikasi ===")
+        if body['cookies'].get('bot_session') == 'mohef123':
+            print("✅ session aktif - bot siap eksekusi")
+        else:
+            print("❌ Session hilang!")
+
+        # # Hit endpoint yang set cookie
+        # response = await client.get("https://httpbin.org/cookies/set?bot_session=abc123&user_id=42")
         
-        print("=== COOKIES TERSIMPAN ===")
-        print(dict(client.cookies))
+        # print("=== COOKIES TERSIMPAN ===")
+        # print(dict(client.cookies))
 
 asyncio.run(main())
